8b06ef1d0e180d0c9613ccb130096c78a3c46abe
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2016-02-22 19:22:12 +0200

    more content to example script

--- example.swift
+++ example.swift
@@ -1,3 +1,29 @@
-let myVar = "Cleveland"
+import Foundation
+import Glibc
 
+// Swift example
+
+let myVar = "Cleveland"
 print("Hello \(myVar)!")
+
+let oneFiveTenArray = [1, 5, 10]
+let timesTwoClosure = {$0 * 2}
+let newArray = oneFiveTenArray.map(timesTwoClosure)
+print("\(oneFiveTenArray) * 2 => \(newArray)")
+
+// Objective-C Foundation Framework example
+
+let date = NSDate()
+let dateFormatter = NSDateFormatter()
+dateFormatter.locale = NSLocale(localeIdentifier: "fi_FI")
+dateFormatter.dateStyle = .FullStyle
+let localizedDate = dateFormatter.stringFromDate(date)
+
+print("Localized date: \(localizedDate)")
+
+// GNU C Library example
+
+// Random number:
+let randNumber = random()
+
+print("Random number: \(randNumber)")
