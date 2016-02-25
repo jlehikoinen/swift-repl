dec57a4e86be4c76fb9ad4b7d6be882609e1c5c9
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2016-02-25 07:17:45 +0200

    Added funcs to example script

--- example.swift
+++ example.swift
@@ -2,29 +2,39 @@ import Foundation
 import Glibc
 
 // Swift example
-print("\n// Swift example //")
+func swift_example() {
+    print("\n// Swift example //")
 
-let myVar = "Cleveland"
-print("Hello \(myVar)!")
+    let myVar = "Cleveland"
+    print("Hello \(myVar)!")
 
-let oneFiveTenArray = [1, 5, 10]
-let timesTwoClosure = {$0 * 2}
-let newArray = oneFiveTenArray.map(timesTwoClosure)
-print("\(oneFiveTenArray) * 2 => \(newArray)")
+    let oneFiveTenArray = [1, 5, 10]
+    let timesTwoClosure = {$0 * 2}
+    let newArray = oneFiveTenArray.map(timesTwoClosure)
+    print("\(oneFiveTenArray) * 2 => \(newArray)")
+}
 
 // Objective-C Foundation Framework example
-print("\n// Obj-C Foundation Framework example //")
+func objc_example() {
+    print("\n// Obj-C Foundation Framework example //")
 
-let date = NSDate()
-let dateFormatter = NSDateFormatter()
-dateFormatter.locale = NSLocale(localeIdentifier: "fi_FI")
-dateFormatter.dateStyle = .FullStyle
-let localizedDate = dateFormatter.stringFromDate(date)
-print("Localized date: \(localizedDate)")
+    let date = NSDate()
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.locale = NSLocale(localeIdentifier: "fi_FI")
+    dateFormatter.dateStyle = .FullStyle
+    let localizedDate = dateFormatter.stringFromDate(date)
+    print("Localized date: \(localizedDate)")
+}
 
 // GNU C Library example
-print("\n// GNU C Library example //")
+func glibc_example() {
+    print("\n// GNU C Library example //")
 
-let randNumber = random()
-print("Random number: \(randNumber)")
-print("")
+    let randNumber = random()
+    print("Random number: \(randNumber)")
+    print("")
+}
+
+swift_example()
+objc_example()
+glibc_example()
